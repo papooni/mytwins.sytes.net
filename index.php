378dbc7b8fa80702a7c6b0aa99404005f499166c
Vasco Sousa <baskito33@hotmail.com>
2017-04-11 17:49:08 +0100

    ?>

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ if(!empty($scanned_directory)) {
     function nextAudioNode() {
         var element = document.getElementById('musica');
         /*element.src = "music/shape_of_you.mp3";*/
-        element.src = "music/" + elements.indexOf();
+        element.src = "music/" + elements.indexOf(i);
         element.play();
     }
 </script>
