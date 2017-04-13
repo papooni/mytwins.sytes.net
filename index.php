6e7b247cbae92aef717d2ea7d009680d894e1a55
Vasco Sousa <baskito33@hotmail.com>
2017-04-13 14:57:07 +0100

    ff

--- index.php
+++ index.php
@@ -82,17 +82,17 @@
         $count = 0;
         if (!empty($scanned_directory)) {
             foreach ($scanned_directory as $key => $ficheiro) {
-                $file = pathinfo($ficheiro);
+                //$file = pathinfo($ficheiro);
                 echo $ficheiro . '<br>';
-                echo $dir  . '<br>';
-                echo $scanned_directory . '<br>';
+                //echo $dir  . '<br>';
+                //echo $scanned_directory . '<br>';
                // if ($file['extension'] == 'jpg'){ //|| ($file_parts['extension'] == 'png')){ //&& filesize($ficheiro) > 100 && strcmp($file_parts['filename'], 'bg-default') != 0) {
                    $count = $count + 1;
               /*  <i class='img' data-src='./img/<?php $ficheiro; ?>' ></i>;*/
                //}
             }
         }
-        echo '<h1>Nr de Ficheiros Aceitáveis -> </h1>'.$count;
+       // echo '<h1>Nr de Ficheiros Aceitáveis -> </h1>'.$count;
         ?>
 
     </div>
