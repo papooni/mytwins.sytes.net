7a650deb79e785c98965fba52ab43e7ab693b09c
Vasco Sousa <baskito33@hotmail.com>
2017-04-13 14:15:52 +0100

    ss

--- index.php
+++ index.php
@@ -79,14 +79,17 @@
         <?php
         $dir = "/home/forge/mytwins.sytes.net/img/";
         $scanned_directory = array_diff(scandir($dir), array('..', '.'));
+        $count = 0;
         if (!empty($scanned_directory)) {
             foreach ($scanned_directory as $key => $ficheiro) {
                 $file_parts = pathinfo($ficheiro);
                 if (($file_parts['extension'] == 'jpg' || $file_parts['extension'] == 'png') && filesize($ficheiro) > 100 && strcmp($file_parts['filename'], 'bg-default') != 0) {
-                   ?> <i class='img' data-src='./img/<?php $ficheiro; ?>' ></i>;<?
-                }
+                   $count = $count + 1;
+              /*  <i class='img' data-src='./img/<?php $ficheiro; ?>' ></i>;*/
+               }
             }
         }
+        echo '<h1>Nr de Ficheiros Aceitáveis -> </h1>'.$count;
         ?>
 
     </div>
