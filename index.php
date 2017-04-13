7750592150c3f916885f94d77b09dc7d16590f57
Vasco Sousa <baskito33@hotmail.com>
2017-04-13 12:04:41 +0100

    te

--- index.php
+++ index.php
@@ -82,7 +82,7 @@
         if (!empty($scanned_directory)) {
             foreach ($scanned_directory as $key => $ficheiro) {
                 $file_parts = pathinfo($ficheiro);
-                if (($file_parts['extension'] == 'jpg' || $file_parts['extension'] == 'png') && filesize($ficheiro) > 100 && strcmp($file_parts['filename'],'bg-default') != 0) {
+                if (($file_parts['extension'] == 'jpg' || $file_parts['extension'] == 'png')){ //&& filesize($ficheiro) > 100 && strcmp($file_parts['filename'],'bg-default') != 0) {
                     echo "<i class='img' data-src='./img/$ficheiro'></i>";
                 }
             }
