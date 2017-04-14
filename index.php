658e0ece3018bf50c0de0e6597beb285fe0937d2
papooni <vascosousa@MacBook-Pro-2.home>
2017-04-14 17:54:05 +0100

    strpos

--- index.php
+++ index.php
@@ -85,7 +85,7 @@
             foreach ($scanned_directory as $key => $ficheiro) {
                 $file = pathinfo($ficheiro);
                 //echo 'file ---- '. print_r($file);
-                if (($file['extension'] == 'jpg' || $file['extension'] == 'png' ) && !is_dir($ficheiro) && filesize($dir . $ficheiro) > 1000 && strcmp($file['filename'], 'bg-default') != 0 && strcmp($file['filename'], 'git') != 0){
+                if (($file['extension'] == 'jpg' || $file['extension'] == 'png' ) && !is_dir($ficheiro) && filesize($dir . $ficheiro) > 10000 && strpos($file['filename'], 'bg-default') !== false && strpos($file['filename'], 'git') != false){
                 echo ' >>>> '.$ficheiro . ' <<<< <br>';
                 //echo $dir  . '<br>';
                 //echo $scanned_directory . '<br>';
