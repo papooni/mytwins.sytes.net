1900b5f21728fe88709d02246bf6e35442ff33c2
papooni <vascosousa@MacBook-Pro-2.local>
2017-05-06 11:50:38 +0100

    jpg

--- index.php
+++ index.php
@@ -85,7 +85,7 @@
             foreach ($scanned_directory as $key => $ficheiro) {
                 $file = pathinfo($ficheiro);
                 //echo 'file ---- '. print_r($file);
-                if (($file['extension'] == 'jpg' || $file['extension'] == 'png' ) && !is_dir($ficheiro) ){
+                if (($file['extension'] == 'jpg' || $file['extension'] == 'JPG' || $file['extension'] == 'png' ) && !is_dir($ficheiro) ){
                  $count = $count + 1;
                    ?>
 
