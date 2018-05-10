dbd891e58274ec7508c463ed149da6dbb3187f37
Vasco Sousa <baskito33@hotmail.com>
2018-05-10 13:04:21 +0100

    img echo

--- index.php
+++ index.php
@@ -87,7 +87,7 @@
                 //echo 'file ---- '. print_r($file);
                 if (($file['extension'] == 'jpg' || $file['extension'] == 'JPG' || $file['extension'] == 'png' ) && !is_dir($ficheiro) ){
                  $count = $count + 1;
-                echo '<i class="img" data-src="./img-background/'.$ficheiro.'" ></i>';
+                <i class='img' data-src="./img-background/<?php echo $ficheiro; ?>" ></i>
                }
             }
         }else{
